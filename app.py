b6f4726db13744a10b707359f57d169c39f8b8ae
Ryan <ryanjwin2020@gmail.com>
2023-04-16 20:59:10 -0600

    sign in works with database

--- app.py
+++ app.py
@@ -3,15 +3,62 @@
 # -- Import section --
 from flask import Flask
 from flask import render_template
-# from flask import request
+from flask_pymongo import PyMongo
+from flask_login import LoginManager, login_user, login_required, logout_user
+from user import User
+import secrets
+from flask import request, redirect, url_for
 
 
 # -- Initialization section --
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/'
+mongo = PyMongo(app)
+client = mongo.cx
+db = client['schedulify']
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_user(user_id)
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('login.html')
+
+@app.route('/create-account')
+def create_account():
+    return render_template('create_account.html')
+
+@app.route('/create-user', methods=['GET','POST'])
+def create_user():
+    if request.method == 'GET':
+        return render_template('create_account.html')
+    first = request.form['first']
+    last = request.form['last']
+    email = request.form['email']
+    password = request.form['password']
+
+    new_user = User(first, last, email, password=password)
+    new_user.save()
+    new_user.authenticated = True
+    login_user(new_user)
+
+    return redirect(url_for('select'))
+
+
+@app.route('/home')
+@login_required
+def home():
     return render_template('home.html')
+
+@app.route('/select')
+@login_required
+def select():
+    return render_template('select.html')
---
+++ user.py
@@ -0,0 +1,65 @@
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['schedulify']
+users = db['users']
+
+class User():
+    def __init__(self, first_name, last_name, email, password=None, password_hash=None):
+        self.username = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password_hash = generate_password_hash(password) if password_hash is None else password_hash
+        self.email = email
+        self.uuid = self.generate_uuid()
+        self.authenticated = False
+
+        # make sure id is not already used
+        while users.find_one({'_id': self.uuid}):
+            self.uuid = self.generate_uuid()
+
+    def generate_uuid(self):
+        return str(uuid.uuid4())[:8]
+    
+    # careful with this method!!!
+    def set_uuid(self, id):
+        self.uuid = id
+
+    def save(self):
+
+        users.insert_one({
+            '_id': self.uuid,
+            'username': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'password_hash': self.password_hash,
+            'email': self.email,
+        })
+
+    @staticmethod
+    def get_user(username):
+        return users.find_one({'username': username})
+    
+    @staticmethod
+    def check_password(email, password):
+        user_res = User.get_user(email)
+        if check_password_hash(user_res.password_hash, password):
+            user = User(user_res.email, password_hash=user_res.password_hash)
+            user.set_uuid(user_res._id)
+            user.authenticated = True
+            return user
+        return None
+    
+    def get_id(self):
+        return self.uuid
+    
+    def is_authenticated(self):
+        return self.authenticated
+    
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
